7b163d02330982c7ae406fd020dbfc3c6d3f997a
Shubhanan Sharma <shubhanans@gmail.com>
2025-05-27 20:25:14 +0530

    Add files via upload

---
+++ misc/30 - React + TypeScript/basics-ts/basics.ts
@@ -0,0 +1,59 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+let age: number;
+age = 12;
+
+let userName: string | string[];
+userName = "Max";
+
+let isInstructor: boolean;
+isInstructor = true;
+
+// More complex types
+let hobbies: string[];
+hobbies = ["Sports", "Cooking"];
+
+// type alias
+type Person = {
+  name: string;
+  age: number;
+};
+
+let person: Person;
+
+person = {
+  name: "Max",
+  age: 32,
+};
+// person = {
+//   isEmployee: true
+// };
+
+let people: Person[];
+
+// Type inference
+let course: string | number = "React - The Complete Guide";
+course = 12341;
+
+// Functions & types
+function addNum(a: number, b: number) {
+  return a + b;
+}
+function printOutput(value: any) {
+  console.log(value);
+}
+
+// Generics
+function insertAtBeginning<T>(array: T[], value: T) {
+  // type of array and value should be same
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
+// updatedArray[0].split('');
